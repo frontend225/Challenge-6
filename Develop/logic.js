b4ddfddc09621eddbfe5cfaf604874deb485457c
frontend225 <tfahmed@yahoo.com>
2021-09-25 15:47:36 -0700

    commit

--- Develop/logic.js
+++ Develop/logic.js
@@ -1,182 +1,172 @@
 // tracking variable
 var currentQuestionIndex = 0;
-var time= questions.length *20; 
+var time = questions.length * 20;
 var timerId;
 
-
-// variable for dom elements
-
-var questionsEl= document.getElementById("questions");
-var timerEl= document.getElementById("time");
-var choicesEl= document.getElementById("choices");
+// dom elements variable
+var questionsEl = document.getElementById("questions");
+var timerEl = document.getElementById("time");
+var choicesEl = document.getElementById("choices");
 var submitBtn = document.getElementById("submit");
 var startBtn = document.getElementById("start");
 var initialsEl = document.getElementById("initials");
-var feedbackEl= document.getElementById("feedback");
+var feedbackEl = document.getElementById("feedback");
 
 // sound effects
-
 var sfxRight = new Audio("assets/sfx/correct.wav");
-var sfxWrong= new Audio("assets/sfx/incorrect.wav");
+var sfxWrong = new Audio("assets/sfx/incorrect.wav");
 
-function startQuiz() {}
-    //conceal start screen
-    varstartScreenEl= document.getElementById("start-screen");
-    startScreenEl.setAttribute("class", "hide");
+function startQuiz() {
+  // conceal start screen
+  var startScreenEl = document.getElementById("start-screen");
+  startScreenEl.setAttribute("class", "hide");
 
-    //reveal questions 
-    questionsEl.removeAttribute("class");
+  // reveal questions 
+  questionsEl.removeAttribute("class");
 
-    // begin timer
-    timerId= setInterval(clockTick, 2000);
+  // begin timer
+  timerId = setInterval(clockTick, 2000);
 
-    //display start time
-timerEl.textContent= time;
+  // display start time
+  timerEl.textContent = time;
 
-getQuestion();
-
-
-
-function clockTick () {
-    // updates the time
-    time--;
-    timerEl.textContent = time;
-
-
-    //determines if no time is left for the user
-    if (time <= 0) {
-        quizEnd();
-    }
+  getQuestion();
 }
 
-function getQuestion () {
-    //retrieve current question
-    var currentQuestion = questions(currentQuestionIndex);
+function getQuestion() {
+  // retrieve current question
+  var currentQuestion = questions[currentQuestionIndex];
 
-    //input current question into title
-    var titleEl = document.getElementById("question-title");
-    titleEl.textContent = currentQuestion.title;
+  // input current question into title
+  var titleEl = document.getElementById("question-title");
+  titleEl.textContent = currentQuestion.title;
 
-    //clear old questions
-    choicesEl.innerHTML = "";
-    
-    //loop over choices
-    currentQuestion.choices.forEach(function(choice, i) {
-        //new buttons per choice
-        varchoiceNode = document.createElement("button");
-        choiceNode.setAttribute("class", "choice");
-        choiceNode.setAttribute("value", choice);
+  // remove old questions
+  choicesEl.innerHTML = "";
 
+  // loop over choices
+  currentQuestion.choices.forEach(function(choice, i) {
+    // new buttons per choice
+    var choiceNode = document.createElement("button");
+    choiceNode.setAttribute("class", "choice");
+    choiceNode.setAttribute("value", choice);
 
-choiceNode.textContent = i + 1 + ", " + choice;
-    
+    choiceNode.textContent = i + 1 + ". " + choice;
 
-    //connect click event listener per choice
+    // connect click event listener per choice
     choiceNode.onclick = questionClick;
 
-    //display
-choicesEl.appendChild(choiceNode);
-});
+    // display
+    choicesEl.appendChild(choiceNode);
+  });
 }
 
 function questionClick() {
-    //determine if user's answer is incorrect
-    if (this.value !== questions[currentQuestionIndex].answer) {
-        // time penalty
-        time -=10;
+  // determine if user's answer is incorrect
+  if (this.value !== questions[currentQuestionIndex].answer) {
+    // time penalty
+    time -= 15;
 
-        if (time < 0) {
-            time = 0;
-        }
+    if (time < 0) {
+      time = 0;
+    }
 
-            //new time
-            timerEl.textContent = time;
+    // new time
+    timerEl.textContent = time;
 
-            //sound effect for incorrect answers
-            sfxWrong.play();
+    // sound effect for incorrect answers
+    sfxWrong.play();
 
-            feedbackEl.textContent = "Wrong!";
-        } else {
-            //sound effect for correct answers
-            sfxRight.play();
+    feedbackEl.textContent = "Wrong!";
+  } else {
+    // sound effect for correct answers
+    sfxRight.play();
 
-            feedbackEl.textContent = "Correct!";
-        }
-        }
-        //display answer feedback for half a second
-        feedbackEl.setAttribute("class", "feedback");
-        setTimeout(function() {
-            feedbackEl.setAttribute("class", "feedback hide");
-}, 2000);
+    feedbackEl.textContent = "Correct!";
+  }
 
-//proceed to next question
-currentQuestionIndex++;
+  // display answer feedback for half a second
+  feedbackEl.setAttribute("class", "feedback");
+  setTimeout(function() {
+    feedbackEl.setAttribute("class", "feedback hide");
+  }, 1000);
 
-//determine if out of questions
-if (currentQuestionIndex === questions.length) {
-quizend();
-} else {
-getQuestion();
+  // proceed to next question
+  currentQuestionIndex++;
+
+  // determine if out of questions
+  if (currentQuestionIndex === questions.length) {
+    quizEnd();
+  } else {
+    getQuestion();
+  }
 }
+
 function quizEnd() {
-    //stop timer
-    clearInterval(timerId);
+  // stop timer
+  clearInterval(timerId);
 
-    //display end screen
-    var endScreenEl = document.getElementById("end-screen");
-    endScreenEl.removeAttribute("class");
+  // display end screen
+  var endScreenEl = document.getElementById("end-screen");
+  endScreenEl.removeAttribute("class");
 
-    //display final score
-    var finalScoreEl = document.getElementById("final-score");
-   finalScoreEl.textcontent = time;
+  // display final score
+  var finalScoreEl = document.getElementById("final-score");
+  finalScoreEl.textContent = time;
 
-   //conceal questions
-   questionsEl.setAttribute("class", "hide");
+  // conceal questions
+  questionsEl.setAttribute("class", "hide");
+}
 
+function clockTick() {
+  // update time
+  time--;
+  timerEl.textContent = time;
+
+  // determine if user ran out of time
+  if (time <= 0) {
+    quizEnd();
+  }
 }
 
 function saveHighscore() {
-    //retrieve input box value
-    var initials = initialsEl.value.trim();
+  // retrieve input box value
+  var initials = initialsEl.value.trim();
 
-    //ensure value is not empty
-    if (initials !== "") {
-
-
-    //retrieve saved scores from local storage or create an empty array if unavailable
-    
-    //var highscores = [];
+  // ensure value is not empty
+  if (initials !== "") {
+    // retrieve saved scores from local storage or create an empty array if unavailable
     var highscores =
-    JSON.parse(window.localStorage.getItem("highscores")) || [];
+      JSON.parse(window.localStorage.getItem("highscores")) || [];
 
-    //format new score for new user
+    // format new score for new user
     var newScore = {
-        score: time,
-        intials: initials
+      score: time,
+      initials: initials
     };
 
-    //save to localstorage
+    // save 
     highscores.push(newScore);
     window.localStorage.setItem("highscores", JSON.stringify(highscores));
 
-    //proceed to next page
-    window.location.href ="highscores.html";
-
-}
+    // proceed to next page
+    window.location.href = "highscores.html";
+  }
 }
 
 function checkForEnter(event) {
-    //"13" for enter key
-    if (event.key === "Enter") {
-        saveHighscore();
-
-    }
+  // "13" for enter key
+  if (event.key === "Enter") {
+    saveHighscore();
+  }
 }
-//button to submit initials
+
+// button that submits initials
 submitBtn.onclick = saveHighscore;
 
-//button to start quiz
+// button that starts quiz
 startBtn.onclick = startQuiz;
- 
+
 initialsEl.onkeyup = checkForEnter;
+
 
--- Develop/questions.js
+++ Develop/questions.js
@@ -1,32 +1,34 @@
 var questions = [
 
-    {
-        title: "Data types exclude: ",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
-    },
+  {
+      title: "Data types exclude: ",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+  },
 
-    {
-        title: "Data types exclude: ",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
-    },
-    
-    {
-        title: "Data types exclude: ",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
-    },
-    
-    {
-        title: "Data types exclude: ",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
-    },
-    
-    {
-        title: "Data types exclude: ",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
-    },
+  {
+      title: "Data types exclude: ",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+  },
+  
+  {
+      title: "Data types exclude: ",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+  },
+  
+  {
+      title: "Data types exclude: ",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+  },
+  
+  {
+      title: "Data types exclude: ",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+  },
 ]
+
+  
